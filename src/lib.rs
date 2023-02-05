38af6ada3581819908716cf180daa2b71bead994
Will Pringle <willkantorpringle+1@gmail.com>
2023-02-05 16:39:56 -0500

    add lot

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
@@ -32,4 +34,30 @@ pub fn add(a: u32, b: u32) -> u32 {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Example {
+    pub field1: HashMap<u32, String>,
+    pub field2: Vec<Vec<f32>>,
+    pub field3: [f32; 4],
+}
+
+#[wasm_bindgen]
+pub fn send_example_to_js() -> JsValue {
+    let mut field1 = HashMap::new();
+    field1.insert(0, String::from("ex"));
+    let example = Example {
+        field1,
+        field2: vec![vec![1., 2.], vec![3., 4.]],
+        field3: [1., 2., 3., 4.]
+    };
+
+    JsValue::from_serde(&example).unwrap()
+}
+
+
+#[wasm_bindgen]
+pub fn receive_example_from_js(val: &JsValue) {
+    let example: Example = val.into_serde().unwrap();
 }
