12a6987e6b3745b4120c9065bf799ecf7a2e74a8
Will Pringle <willkantorpringle+1@gmail.com>
2023-03-25 09:20:30 -0400

    Revert "use react for frontend"
    This reverts commit 014f33075e83d5c4a11b190232a92aaacef18868.

--- react/Message.jsx
+++
@@ -1,5 +0,0 @@
-function Message() {
-    return <h1>Hello Pringle</h1>;
-}
-
-export default Message;
--- react/components/Alert.jsx
+++
@@ -1,9 +0,0 @@
-//import React from 'react';
-
-const Alert = (props) => {
-    return (
-        <div className="alert alert-primary">{props.children}</div>
-    )
-}
-
-export default Alert;
