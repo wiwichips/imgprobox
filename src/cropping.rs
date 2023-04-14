76adb3a6b018ac89e3975de8527b9d5c9d17175d
Will Pringle <willkantorpringle+1@gmail.com>
2023-04-14 13:45:59 -0400

    Add comments to cropping

--- src/cropping.rs
+++ src/cropping.rs
@@ -1,12 +1,14 @@
 use crate::image::Image;
 
-// cropping
+/// Crop an image and return a new cropped image object
+/// Pass in a left top corner point and a right bottom point of the reigion to crop
 pub fn crop_image(img_src: &Image, x1: i32, y1: i32, x2: i32, y2: i32) -> Image {
     let width = x2 - x1;
     let height = y2 - y1;
     let mut data = vec![255; (width * height * 4) as usize];
     let mut i = 0;
 
+    // iterate through the region and copy the pixels to the new output image
     for y in y1..y2 {
         for x in x1..x2 {
             let (r,g,b) = img_src.get_pixel_intensity(x, y);
@@ -18,5 +20,3 @@ pub fn crop_image(img_src: &Image, x1: i32, y1: i32, x2: i32, y2: i32) -> Image 
     }
     return Image::new(data, width, height);
 }
-
-
